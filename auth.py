a51cf9816e4a391c783fd347d3643ece5d9e57f1
JurriaanG <jurriaang93@gmail.com>
2024-07-16 22:44:39 +0200

    Update auth.py
    ###

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-consumer_key        = 'zLvhphfclDKZjbP3aCdRPpoqL'
-consumer_secret     = 'NNwYKpaLkx7isyyt1LaydwP4sxEIiqMUT5AfUxqFNDeqr56V8b'
-access_token        = '1008460482901135365-rkwV1lOPe2YP5GpxPKuEQxOa0ozf0G'
-access_token_secret = 'I0zzaFN8Z94cs7iqmQU5vUQtNebdSr7AvPqfSzFOp79LA'
+consumer_key        = '###'
+consumer_secret     = '###'
+access_token        = '###'
+access_token_secret = '###'
